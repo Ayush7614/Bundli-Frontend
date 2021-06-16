93eb03c73afc7a50e8664a513d43611c88bf9612
vasudev19126 <vasudev19126@iiitd.ac.in>
2021-06-16 23:16:33 +0530

    remove catch block

--- Github Profiles/script.js
+++ Github Profiles/script.js
@@ -10,10 +10,6 @@ async function getUser(username) {
 
         createUserCard(data)
         getRepos(username)
-    } catch(err) {
-        if(err.response.status == 404) {
-            createErrorCard('No profile with this username')
-        }
     }
 }
 
