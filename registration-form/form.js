d46fb6c35be0f26b5944ae43575590367a002ddb
pranjay-poddar <pranjaypoddar@outlook.com>
2021-06-26 17:38:59 +0530

    email checks added

--- registration-form/form.js
+++ registration-form/form.js
@@ -18,3 +18,34 @@ function allLetter(inputtxt) {
         return false;
     }
 }
+
+//----name length check-------------------------------------------------------------------------------
+function strleng(inputtxt) {
+    var field = inputtxt.value;
+    var mnlen = 5;
+
+
+    if (field.length < mnlen) {
+        var x = document.getElementById("click");
+        swal("Username must be atleast 5 characters long!");
+        x.play();
+        return false;
+    } else {
+        return true;
+    }
+}
+//---email check-----------------------------------------------------------------------------------------
+function ValidateEmail(inputText) {
+    var mailformat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    if (inputText.value.match(mailformat)) {
+        document.form1.email.focus();
+        return true;
+    } else {
+        setTimeout(() => {
+            swal("You have entered an invalid email address!");
+        }, 500);
+
+        document.form1.email.focus();
+        return false;
+    }
+}
