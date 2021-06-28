9787a021c689aa377b14419bf9d58edc36113f17
pranjay-poddar <pranjaypoddar@outlook.com>
2021-06-28 12:21:58 +0530

    event validation added

--- registration-form/jquery.confetti.js
+++ registration-form/jquery.confetti.js
@@ -68,3 +68,78 @@
                     });
 
                 }
+
+                // $(document).ready(init());
+
+                function InitializeButton() {
+                    $('#startConfetti').click(InitializeConfetti);
+                    $('#stopConfetti').click(DeactivateConfetti);
+                    $('#restartConfetti').click(RestartConfetti);
+                }
+
+                function SetGlobals() {
+                    $('body').append('<canvas id="confettiCanvas" style="position:absolute;top:0;right:0;display:none;z-index:99;"></canvas>');
+                    canvas = document.getElementById("confettiCanvas");
+                    ctx = canvas.getContext("2d");
+                    W = window.innerWidth;
+                    H = window.innerHeight;
+                    canvas.width = W;
+                    canvas.height = H;
+                }
+
+                function InitializeConfetti() {
+                    canvas.style.display = 'block';
+                    particles = [];
+                    animationComplete = false;
+                    for (var i = 0; i < mp; i++) {
+                        var particleColor = particleColors.getColor();
+                        particles.push(new confettiParticle(particleColor));
+                    }
+                    StartConfetti();
+                }
+
+                function Draw() {
+                    ctx.clearRect(0, 0, W, H);
+                    var results = [];
+                    for (var i = 0; i < mp; i++) {
+                        (function(j) {
+                            results.push(particles[j].draw());
+                        })(i);
+                    }
+                    Update();
+
+                    return results;
+                }
+
+                function RandomFromTo(from, to) {
+                    return Math.floor(Math.random() * (to - from + 1) + from);
+                }
+
+
+                function Update() {
+                    var remainingFlakes = 0;
+                    var particle;
+                    angle += 0.01;
+                    tiltAngle += 0.1;
+
+                    for (var i = 0; i < mp; i++) {
+                        particle = particles[i];
+                        if (animationComplete) return;
+
+                        if (!confettiActive && particle.y < -15) {
+                            particle.y = H + 100;
+                            continue;
+                        }
+
+                        stepParticle(particle, i);
+
+                        if (particle.y <= H) {
+                            remainingFlakes++;
+                        }
+                        CheckForReposition(particle, i);
+                    }
+
+                    if (remainingFlakes === 0) {
+                        StopConfetti();
+                    }
+                }
