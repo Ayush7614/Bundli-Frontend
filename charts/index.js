34e15ee70eaf0eebcf99250bc0c5e17505650a0e
pranjay-poddar <pranjaypoddar@outlook.com>
2021-07-15 16:46:35 +0530

    added doughnut chart js

--- charts/index.js
+++ charts/index.js
@@ -59,3 +59,25 @@ var myChart = new Chart(ctxpie, {
         }]
     }
 });
+
+var ctxpie = document.getElementById('doughnutchart').getContext('2d');
+var myChart = new Chart(ctxpie, {
+    type: 'doughnut',
+    data: {
+        labels: [
+            'Red',
+            'Blue',
+            'Yellow'
+        ],
+        datasets: [{
+            label: 'My First Dataset',
+            data: [300, 50, 100],
+            backgroundColor: [
+                'rgb(255, 99, 132)',
+                'rgb(54, 162, 235)',
+                'rgb(255, 205, 86)'
+            ],
+            hoverOffset: 4
+        }]
+    }
+});
