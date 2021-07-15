f67011868d2311b7a78d05974f824215bfebc103
pranjay-poddar <pranjaypoddar@outlook.com>
2021-07-15 16:46:04 +0530

    added pie chart js

--- charts/index.js
+++ charts/index.js
@@ -37,3 +37,25 @@ var myChart = new Chart(ctx, {
         }
     }
 });
+
+var ctxpie = document.getElementById('piechart').getContext('2d');
+var myChart = new Chart(ctxpie, {
+    type: 'pie',
+    data: {
+        labels: [
+            'Red',
+            'Blue',
+            'Yellow'
+        ],
+        datasets: [{
+            label: 'My First Dataset',
+            data: [300, 50, 100],
+            backgroundColor: [
+                'rgb(255, 99, 132)',
+                'rgb(54, 162, 235)',
+                'rgb(255, 205, 86)'
+            ],
+            hoverOffset: 4
+        }]
+    }
+});
