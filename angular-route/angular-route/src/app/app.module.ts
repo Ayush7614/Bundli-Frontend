d2cf582839d384ec5256a9f1925fb04578e65f92
pranjay-poddar <pranjaypoddar@outlook.com>
2021-07-20 18:32:06 +0530

    navbar component added

--- angular-route/angular-route/src/app/app.module.ts
+++ angular-route/angular-route/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
