c71fb8e90756c1040b995bf68672d77a3e7d45f1
pranjay-poddar <pranjaypoddar@outlook.com>
2021-07-20 18:32:48 +0530

    container1 component added

--- angular-route/angular-route/src/app/app.module.ts
+++ angular-route/angular-route/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { Container1Component } from './container1/container1.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    Container1Component
   ],
   imports: [
     BrowserModule,
