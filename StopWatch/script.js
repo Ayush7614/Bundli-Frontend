db6894877dd2c1015b408e0205fc4273fe3736ef
Ushmita Dutta <76655696+ushmita4@users.noreply.github.com>
2021-07-31 15:29:46 +0530

    Create script.js

---
+++ StopWatch/script.js
@@ -0,0 +1,72 @@
+function timeToString(time) {
+    let diffInHrs = time / 3600000;
+    let hh = Math.floor(diffInHrs);
+  
+    let diffInMin = (diffInHrs - hh) * 60;
+    let mm = Math.floor(diffInMin);
+  
+    let diffInSec = (diffInMin - mm) * 60;
+    let ss = Math.floor(diffInSec);
+  
+    let diffInMs = (diffInSec - ss) * 100;
+    let ms = Math.floor(diffInMs);
+  
+    let formattedMM = mm.toString().padStart(2, "0");
+    let formattedSS = ss.toString().padStart(2, "0");
+    let formattedMS = ms.toString().padStart(2, "0");
+  
+    return `${formattedMM}:${formattedSS}:${formattedMS}`;
+  }
+  
+  // Declare variables to use in our functions below
+  
+  let startTime;
+  let elapsedTime = 0;
+  let timerInterval;
+  
+  // Create function to modify innerHTML
+  
+  function print(txt) {
+    document.getElementById("display").innerHTML = txt;
+  }
+  
+  // Create "start", "pause" and "reset" functions
+  
+  function start() {
+    startTime = Date.now() - elapsedTime;
+    timerInterval = setInterval(function printTime() {
+      elapsedTime = Date.now() - startTime;
+      print(timeToString(elapsedTime));
+    }, 10);
+    showButton("PAUSE");
+  }
+  
+  function pause() {
+    clearInterval(timerInterval);
+    showButton("PLAY");
+  }
+  
+  function reset() {
+    clearInterval(timerInterval);
+    print("00:00:00");
+    elapsedTime = 0;
+    showButton("PLAY");
+  }
+  
+  // Create function to display buttons
+  
+  function showButton(buttonKey) {
+    const buttonToShow = buttonKey === "PLAY" ? playButton : pauseButton;
+    const buttonToHide = buttonKey === "PLAY" ? pauseButton : playButton;
+    buttonToShow.style.display = "block";
+    buttonToHide.style.display = "none";
+  }
+  // Create event listeners
+  
+  let playButton = document.getElementById("playButton");
+  let pauseButton = document.getElementById("pauseButton");
+  let resetButton = document.getElementById("resetButton");
+  
+  playButton.addEventListener("click", start);
+  pauseButton.addEventListener("click", pause);
+  resetButton.addEventListener("click", reset);
