4ac0fe06a7d848ef75f0b7a6f52a2a9db7c73302
Hariket Sukeshkumar Sheth <hariket.sukeshkumar2020@gmail.com>
2022-05-26 00:15:50 +0530

    Added Files

---
+++ Disney Plus Clone/README.md
@@ -0,0 +1,22 @@
+# Disney Plus UI Clone :
+## Tech Stack Used: HTML5,CSS3,Javascript
+---
+
+## Looks Like:
+![image](https://user-images.githubusercontent.com/77873383/162864663-3ce0a076-eda4-437a-a169-e9f192c07024.png)
+
+
+----
+## Video Demo:
+
+
+https://user-images.githubusercontent.com/77873383/162864911-499885b2-87da-4b45-b2bd-34393e3589bb.mp4
+
+
+---
+
+## Deployed link:
+
+https://mrjoy832.github.io/Disney-/
+
+---
---
+++ Disney Plus Clone/script.js
@@ -0,0 +1,27 @@
+let movies = [
+    {
+        name: 'falcon and the winter soldier',
+        des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
+        image: 'images/slider 2.PNG'
+    },
+    {
+        name: 'loki',
+        des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
+        image: 'images/slider 1.PNG'
+    },
+    {
+        name: 'wanda vision',
+        des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
+        image: 'images/slider 3.PNG'
+    },
+    {
+        name: 'raya and the last dragon',
+        des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
+        image: 'images/slider 4.PNG'
+    },
+    {
+        name: 'luca',
+        des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
+        image: 'images/slider 5.PNG'
+    }
+]
